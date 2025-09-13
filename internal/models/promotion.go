7b0f3793a14b8b2f9f2249cb5d5df5b1d362ca40
Estivkefelegn <kefelegnestifanos0@gmail.com>
2025-09-13 19:40:19 +0300

    Changing all id's to uuid field

--- internal/models/promotion.go
+++ internal/models/promotion.go
@@ -7,17 +7,17 @@ import (
 )
 
 type Promotion struct {
-	ID          int             `josn:"id,omiempty" db:"id,omitempty"`
+	ID          string          `josn:"id,omiempty" db:"id,omitempty"`
 	Description string          `json:"description,omitempty" db:"description,omiempty"`
 	Discount    decimal.Decimal `json:"discount,omitempty" db:"discount,omitempty"`
 }
 
 // for product and Promotion many to many relationship
 type PromotionProduct struct {
-    ID          int        `json:"id" db:"id"`
-    PromotionID int        `json:"promotion_id" db:"promotion_id"`
-    ProductID   int        `json:"product_id" db:"product_id"`
-    CreatedAt   time.Time  `json:"created_at" db:"created_at"`
-    Promotion   *Promotion `json:"promotion,omitempty" db:"-"` 
-    Product     *Product   `json:"product,omitempty" db:"-"`   
+	ID          string     `json:"id" db:"id"`
+	PromotionID string     `json:"promotion_id" db:"promotion_id"`
+	ProductID   string     `json:"product_id" db:"product_id"`
+	CreatedAt   time.Time  `json:"created_at" db:"created_at"`
+	Promotion   *Promotion `json:"promotion,omitempty" db:"-"`
+	Product     *Product   `json:"product,omitempty" db:"-"`
 }
